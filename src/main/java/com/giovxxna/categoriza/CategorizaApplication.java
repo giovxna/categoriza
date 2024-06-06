847908fd43771a34aba63ad7eda78402c39eaeb2
giovxna <gi.siqueira0202@gmail.com>
2024-06-06 19:30:37 -0300

    :tada: Commit inicial

---
+++ src/main/java/com/giovxxna/categoriza/CategorizaApplication.java
@@ -0,0 +1,13 @@
+package com.giovxxna.categoriza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategorizaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CategorizaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/giovxxna/categoriza/CategorizaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giovxxna.categoriza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategorizaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
