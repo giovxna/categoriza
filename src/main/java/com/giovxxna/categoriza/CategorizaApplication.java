6ac44b5cfe6b08f5a0db832c81c18187362a07db
giovxna <gi.siqueira0202@gmail.com>
2024-06-06 20:20:09 -0300

    config. MongoDB

--- src/main/java/com/giovxxna/categoriza/CategorizaApplication.java
+++ src/main/java/com/giovxxna/categoriza/CategorizaApplication.java
@@ -1,9 +1,12 @@
 package com.giovxxna.categoriza;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories()
 public class CategorizaApplication {
 
 	public static void main(String[] args) {
